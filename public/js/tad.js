7e35d2c8035aafcbd2eb33264d07b1dbc2e5e395
Tanya <tanyadiit7@gmail.com>
2022-04-15 19:38:06 +0530

    Finished Truth and Dare

--- public/js/tad.js
+++ public/js/tad.js
@@ -14,8 +14,6 @@ function pickName() {
     display.textContent = nameArray[index];
 }
 
-//Truth
-
 var truths = [
     "When was the last time you lied?",
     "When was the last time you cried?",
@@ -37,43 +35,173 @@ var truths = [
     "What's your biggest insecurity?"
 ]
 
-const questions = document.getElementById('truth-questions')
+var dares = [
+    "Show the most embarrassing photo on your phone",
+    "Show the last five people you texted and what the messages said",
+    "Let the rest of the group DM someone from your Instagram account",
+    "Eat a raw piece of garlic",
+    "Do 100 squats",
+    "Keep three ice cubes in your mouth until they melt",
+    "Say something dirty to the person on your left",
+    "Give a foot massage to the person on your right",
+    "Put 10 different available liquids into a cup and drink it",
+    "Yell out the first word that comes to your mind",
+    "Give a lap dance to someone of your choice",
+    "Remove four items of clothing",
+    "Like the first 15 posts on your Facebook newsfeed",
+    "Eat a spoonful of mustard",
+    "Keep your eyes closed until it's your go again",
+    "Empty out your wallet/purse and show everyone what's inside",
+    "Pretend to be the person to your right for 10 minutes",
+    "Eat a snack without using your hands"
+]
 
-function togglePopup() {
+var nevers = [
+    "Never have I ever played hooky from school or work",
+    "Never have I ever stolen anything",
+    "Never have I ever missed a flight",
+    "Never have I ever drunk-dialed my ex",
+    "Never have I ever rode a motorcycle",
+    "Never have I ever lost a bet",
+    "Never have I ever gotten lost alone in a foreign country",
+    "Never have I ever bribed someone",
+    "Never have I ever gone skinny-dipping",
+    "Never have I ever cheated on someone",
+    "Never have I ever sang karaoke",
+    "Never have I ever broken a bone",
+    "Never have I ever lived alone",
+    "Never have I ever been on a yacht",
+    "Never have I ever been on TV",
+    "Never have I ever been on a blind date",
+    "Never have I ever lied to law enforcement",
+    "Never have I ever gotten a tattoo"
+]
+
+const questions = document.getElementById('truth-questions')
+const dareTasks = document.getElementById('dares')
+const neverHave = document.getElementById('never-questions')
+
+/**************  POP UP DISPLAY FUNCTIONS  ***************/
+
+//truth questions 
+function toggleTruth() {
     questions.innerHTML = " ";
     for (truth of truths) {
         const q = document.createElement("button");
         q.textContent = truth;
         questions.appendChild(q);
     }
-    document.getElementById("popup-1").classList.toggle("active");
+    document.getElementById("truth").classList.toggle("active");
 }
 
+//dare tasks
+function toggleDare() {
+    dareTasks.innerHTML = " ";
+    for (dare of dares) {
+        const d = document.createElement("button");
+        d.textContent = dare;
+        dareTasks.appendChild(d);
+    }
+    document.getElementById("dare").classList.toggle("active");
+}
+
+//never have I evers
+function toggleNever() {
+    neverHave.innerHTML = " ";
+    for (nev of nevers) {
+        const n = document.createElement("button");
+        n.textContent = nev;
+        neverHave.appendChild(n);
+    }
+    document.getElementById("never").classList.toggle("active");
+}
+
+/****************  DELETE OPTIONS  *****************/
+
+//truth
 questions.addEventListener('click', (e) => {
-    const text = e.target.textContent;
-    const index = truths.indexOf(text);
+    const index = truths.indexOf(e.target.textContent);
     truths.splice(index, 1);
     questions.removeChild(e.target);
 })
 
-const generateButton = document.querySelector('.generate');
+//dare
+dareTasks.addEventListener('click', (e) => {
+    const index = dares.indexOf(e.target.textContent);
+    dares.splice(index, 1);
+    dareTasks.removeChild(e.target);
+})
 
-generateButton.addEventListener('click', () => {
+//never have I evers
+neverHave.addEventListener('click', (e) => {
+    const index = nevers.indexOf(e.target.textContent);
+    nevers.splice(index, 1);
+    neverHave.removeChild(e.target);
+})
+
+/********************  GENERATOR FUNCTIONS  *********************/
+
+//truth
+function generateTruth() {
     const display = document.getElementById('question');
     display.textContent = "";
     let index = Math.floor(Math.random() * truths.length)
     display.textContent = truths[index];
-})
+}
 
-const input = document.getElementById("add");
-const addQuestionButton = document.getElementById('submit-question')
+//Dare
+function generateDare() {
+    const display = document.getElementById('task');
+    display.textContent = "";
+    let index = Math.floor(Math.random() * dares.length)
+    display.textContent = dares[index];
+}
 
-addQuestionButton.addEventListener('click', () => {
-    if (truths.includes(input.value) == 0) {
-        truths.push(input.value);
+//Never Have I ever
+function generateNever() {
+    const display = document.getElementById('neverHaveI');
+    display.textContent = "";
+    let index = Math.floor(Math.random() * nevers.length)
+    display.textContent = nevers[index];
+}
+
+/*********************  CUSTOM ADDERS  ***********************/
+
+//truth
+const inputT = document.getElementById("addTruth");
+
+function addQuestion() {
+    if (truths.includes(inputT.value) == 0) {
+        truths.push(inputT.value);
         const q = document.createElement("button");
-        q.textContent = input.value;
+        q.textContent = inputT.value;
         questions.appendChild(q);
     }
-    input.value = "";
-})
+    inputT.value = "";
+}
+
+//dare
+const inputD = document.getElementById("addTask");
+
+function addTask() {
+    if (dares.includes(inputD.value) == 0) {
+        dares.push(inputD.value);
+        const q = document.createElement("button");
+        q.textContent = inputD.value;
+        dareTasks.appendChild(q);
+    }
+    inputD.value = "";
+}
+
+//never have I ever
+const inputN = document.getElementById("addNever");
+
+function addNever() {
+    if (nevers.includes(inputN.value) == 0) {
+        nevers.push(inputN.value);
+        const q = document.createElement("button");
+        q.textContent = inputN.value;
+        neverHave.appendChild(q);
+    }
+    inputN.value = "";
+}
