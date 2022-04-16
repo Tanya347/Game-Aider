9255f451e706f1be0f9c071dbe77a42c5d377ffe
Tanya <tanyadiit7@gmail.com>
2022-04-16 12:32:22 +0530

    Cleaned the code and Added colours

--- public/js/song.js
+++ public/js/song.js
@@ -1,38 +1,31 @@
-const colourButton = document.getElementById("colour");
-const h1 = document.querySelector("h1");
-let colourtype;
+// const colourButton = document.getElementById("colour");
+// const h1 = document.querySelector("h1");
+// let colourtype;
 
-function generateColor() {
-    let r = Math.floor(Math.random() * 255) + 1;
-    let g = Math.floor(Math.random() * 255) + 1;
-    let b = Math.floor(Math.random() * 255) + 1;
+// function generateColor() {
+//     let r = Math.floor(Math.random() * 255) + 1;
+//     let g = Math.floor(Math.random() * 255) + 1;
+//     let b = Math.floor(Math.random() * 255) + 1;
 
-    const hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
-    if (hsp > 127.5)
-        colourtype = "light";
-    else
-        colourtype = "dark";
+//     const hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+//     if (hsp > 127.5)
+//         colourtype = "light";
+//     else
+//         colourtype = "dark";
 
-    return `rgb(${r}, ${g}, ${b})`;
+//     return `rgb(${r}, ${g}, ${b})`;
 
-}
+// }
 
-colourButton.addEventListener("click", function () {
-    const newColor = generateColor();
-    document.body.style.backgroundColor = newColor;
-    h1.textContent = "The color is " + newColor;
-    if (colourtype === "dark")
-        document.body.style.color = "white";
-    else
-        document.body.style.color = "black";
-})
-
-const songs = ["Assets/Song/Baepsae.mp3",
-    "Assets/Song/134340.mp3",
-    "Assets/Song/DNA.mp3",
-    "Assets/Song/Ddaeng.mp3",
-    "Assets/Song/Look Here.mp3"
-]
+// colourButton.addEventListener("click", function () {
+//     const newColor = generateColor();
+//     document.body.style.backgroundColor = newColor;
+//     h1.textContent = "The color is " + newColor;
+//     if (colourtype === "dark")
+//         document.body.style.color = "white";
+//     else
+//         document.body.style.color = "black";
+// })
 
 const s1 = document.getElementById("Song1")
 const s2 = document.getElementById("Song2")
@@ -48,9 +41,9 @@ const p4 = document.getElementById("play4")
 const p5 = document.getElementById("play5")
 const arr_play = [p1, p2, p3, p4, p5]
 
-function playSong1() {
+function mainPlay(s, p) {
     for (let i = 0; i < 5; i++) {
-        if (arr_song[i] == s1)
+        if (arr_song[i] == s)
             continue
         else {
             arr_song[i].pause();
@@ -58,87 +51,32 @@ function playSong1() {
         }
     }
 
-    if (s1.paused) {
-        s1.play();
-        p1.src = "Assets/Song/pause.jpg";
+    document.querySelector("body").classList.toggle("change");
+    if (s.paused) {
+        s.play();
+        p.src = "Assets/Song/pause.jpg";
     } else {
-        s1.pause();
-        p1.src = "Assets/Song/play.jpg";
+        s.pause();
+        p.src = "Assets/Song/play.jpg";
     }
 }
 
-function playSong2() {
-    for (let i = 0; i < 5; i++) {
-        if (arr_song[i] == s2)
-            continue
-        else {
-            arr_song[i].pause();
-            arr_play[i].src = "Assets/Song/play.jpg";
-        }
-    }
+p1.addEventListener('click', () => {
+    mainPlay(s1, p1);
+})
 
-    if (s2.paused) {
-        s2.play();
-        p2.src = "Assets/Song/pause.jpg";
-    } else {
-        s2.pause();
-        p2.src = "Assets/Song/play.jpg";
-    }
-}
+p2.addEventListener('click', () => {
+    mainPlay(s2, p2);
+})
 
-function playSong3() {
-    for (let i = 0; i < 5; i++) {
-        if (arr_song[i] == s3)
-            continue
-        else {
-            arr_song[i].pause();
-            arr_play[i].src = "Assets/Song/play.jpg";
-        }
-    }
+p3.addEventListener('click', () => {
+    mainPlay(s3, p3);
+})
 
-    if (s3.paused) {
-        s3.play();
-        p3.src = "Assets/Song/pause.jpg";
-    } else {
-        s3.pause();
-        p3.src = "Assets/Song/play.jpg";
-    }
-}
+p4.addEventListener('click', () => {
+    mainPlay(s4, p4);
+})
 
-function playSong4() {
-    for (let i = 0; i < 5; i++) {
-        if (arr_song[i] == s4)
-            continue
-        else {
-            arr_song[i].pause();
-            arr_play[i].src = "Assets/Song/play.jpg";
-        }
-    }
-
-    if (s4.paused) {
-        s4.play();
-        p4.src = "Assets/Song/pause.jpg";
-    } else {
-        s4.pause();
-        p4.src = "Assets/Song/play.jpg";
-    }
-}
-
-function playSong5() {
-    for (let i = 0; i < 5; i++) {
-        if (arr_song[i] == s5)
-            continue
-        else {
-            arr_song[i].pause();
-            arr_play[i].src = "Assets/Song/play.jpg";
-        }
-    }
-
-    if (s5.paused) {
-        s5.play();
-        p5.src = "Assets/Song/pause.jpg";
-    } else {
-        s5.pause();
-        p5.src = "Assets/Song/play.jpg";
-    }
-}
+p5.addEventListener('click', () => {
+    mainPlay(s5, p5);
+})
