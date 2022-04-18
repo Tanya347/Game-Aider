3087e17e2aa0ddcc011ceefdae58f3ccdf65e2fe
Tanya <tanyadiit7@gmail.com>
2022-04-18 17:39:27 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 4004;
+const port = process.env.PORT || 4004
 
 //view engine setup
 app.use(express.urlencoded({ extended: true }));
